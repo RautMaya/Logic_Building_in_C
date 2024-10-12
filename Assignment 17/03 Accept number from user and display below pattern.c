8d249c8e4d22b993c208c3d8d770b896e0ae729e
RautMaya <suryawanshimaya28@gmail.com>
2024-10-12 16:17:56 +0530

    Pattern Printing

---
+++ Assignment 17/03 Accept number from user and display below pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void Pattern(int iNo)
+{
+    int i=0;
+
+    for(i=1;i<=iNo;i++)
+    {
+        printf("%d\t",i);
+        printf("*\t");
+    }
+}
+int main()
+{
+    int iValue=0;
+
+    printf("Enter number of elements");
+    scanf("%d",&iValue);
+
+
+    Pattern(iValue);
+
+    return 0;
+}
