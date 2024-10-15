36eef99ee53aa21223222091c8e1ee8d707b556a
RautMaya <suryawanshimaya28@gmail.com>
2024-10-15 19:00:45 +0530

    Pattern Printing

---
+++ Assignment 20/02  Accept number of rows and columns from user and display below pattern.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i=0,j=0,No=1;
+
+    for(i=1;i<=iRow;i++)
+        {
+            for(j=1,No=1;j<=iCol;j++,No++)
+            {
+                if(i%2!=0)
+                {
+                    printf("%d",No*2);
+                }
+                else
+                {
+                    if(No%2==1)
+                    {
+                        printf("%d",No);
+
+                    }
+                    No++;
+
+                }
+            }
+            printf("\n");
+        }
+
+
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    printf("Enter number of rows and columns\n");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
